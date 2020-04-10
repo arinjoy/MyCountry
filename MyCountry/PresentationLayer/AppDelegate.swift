ea8c8f325a3fa72bcd7f88c9b326bcd396e251cc
Arinjoy Biswas <arinjoy.biswas@hotmail.com>
2020-04-10 15:09:58 +1000

    moved main UI related code into presentation layer group

---
+++ MyCountry/PresentationLayer/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  MyCountry
+//
+//  Created by Arinjoy Biswas on 10/4/20.
+//  Copyright © 2020 Arinjoy Biswas. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.backgroundColor = .white
+        
+        
+        window?.rootViewController = UINavigationController(rootViewController: MyCountryViewController())
+        window?.makeKeyAndVisible()
+        return true
+    }
+
+
+}
+
